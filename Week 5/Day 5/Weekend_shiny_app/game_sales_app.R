b42bc4bf90e8b60ec662ea43317ce2891d71667d
DavBradshaw <111652511+DavBradshaw@users.noreply.github.com>
2022-11-04 23:46:40 +0000

    3 tabs with content

--- Week 5/Day 5/Weekend_shiny_app/game_sales_app.R
+++ Week 5/Day 5/Weekend_shiny_app/game_sales_app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(jpeg)
 
 platforms <- CodeClanData::game_sales %>% 
   distinct(platform) %>% 
@@ -11,61 +12,94 @@ genre <- CodeClanData::game_sales %>%
   arrange(genre) %>% 
   pull()
 
+resident_evil_by_year <- CodeClanData::game_sales %>% 
+  filter(str_detect(name, "esident")) %>% 
+  group_by(name) %>% 
+  arrange(year_of_release) %>% 
+  distinct(name) %>% 
+  pull(name)
+
+resident_evil <- CodeClanData::game_sales %>% 
+  filter(str_detect(name, "esident")) %>% 
+  mutate(name = factor(name, levels = resident_evil_by_year)) %>% 
+  group_by(name) %>% 
+  mutate(total_sales = sum(sales)) %>% 
+  mutate(average_user_score = mean(user_score)) %>% 
+  mutate(average_critic_score = mean(critic_score)) %>% 
+  distinct(name, total_sales, average_critic_score, average_user_score)
+
+img <- readJPEG("Umbrella-Corporation-Logo-720x450.jpg")
 
 ui <- fluidPage(
   
-  titlePanel("I'm selective about what games I buy"),
+  titlePanel("What game to buy?"),
   
   tabsetPanel(
-    tabPanel(title = "What kind of game would you like?",
-      fluidRow(
-        column(4,
-               selectInput(
-                 inputId = "platform",
-                 label = "What platform:",
-                 choices = platforms
-               )),
-        
-        column(4,
-               selectInput(
-                 inputId = "genre",
-                 label = "What Genre:",
-                 choices = genre
-               )),
-        
-        column(4,
-               sliderInput(
-                 inputId = "year",
-                 label = "What year:",
-                 min = 1988,
-                 max = 2016,
-                 value = 2000,
-                 sep = NULL
-                 
-               ))
-        
-      ),
-      fluidRow(
-        column(6,
-               tableOutput(outputId = "games_table")),
-        
-        column(6,
-               plotOutput(outputId = "sales_plot"))
-        
-        ),
-      fluidRow(
-        column(6,
-               plotOutput(outputId = "user_plot")),
-        column(6,
-               plotOutput(outputId = "critics_plot"))
-      )
+    tabPanel(title = "I'm open to anything",
+             fluidRow(
+               column(4,
+                      selectInput(
+                        inputId = "platform",
+                        label = "What platform:",
+                        choices = platforms
+                      )),
+               
+               column(4,
+                      selectInput(
+                        inputId = "genre",
+                        label = "What Genre:",
+                        choices = genre
+                      )),
+               
+               column(4,
+                      sliderInput(
+                        inputId = "year",
+                        label = "What year:",
+                        min = 1988,
+                        max = 2016,
+                        value = 2000,
+                        sep = NULL
+                        
+                      ))
+               
+             ),
+             fluidRow(
+               column(6,
+                      "Table showing data for the games selected",
+                      tableOutput(outputId = "games_table")),
+               
+               column(6,
+                      plotOutput(outputId = "sales_plot"))
+               
+             ),
+             fluidRow(
+               column(6,
+                      plotOutput(outputId = "user_plot")),
+               column(6,
+                      plotOutput(outputId = "critics_plot"))
+             )
     ),
     
     
     
     
-    tabPanel(title = "I just want the best",
-             )
+    tabPanel(title = "I only care about Resident Evil games",
+             
+             plotOutput(outputId = "resi_plot"),
+             "now some test?"
+    ),
+    
+    tabPanel(title = "I only care about FIFA games",
+             selectInput(
+               inputId = "release_year",
+               label = "Which year:",
+               choices = c(2000:2016)
+             ),
+             plotOutput(outputId = "fifa_plot"),
+             "Table showing data for the games released in the chosen year",
+             tableOutput(outputId = "fifa_table"),
+             plotOutput(outputId = "fifa_overall")
+    )
   )
   
 )
@@ -78,6 +112,12 @@ server <- function(input, output, session) {
              year_of_release == input$year,
              platform == input$platform)
   )
+  
+  fifa_year <- reactive(CodeClanData::game_sales %>% 
+    filter(str_detect(name, "FIFA"), year_of_release == input$release_year)
+  )
+  
+  
   
   output$games_table <- renderTable(
     year_genre_platform() %>% 
@@ -114,7 +154,44 @@ output$sales_plot <- renderPlot(
         scale_fill_brewer(palette = "Pastel1")
     )
   
+    
+    output$resi_plot <- renderPlot(
+      resident_evil %>% 
+        ggplot()+
+        background_image(img)+
+        geom_point(aes(x = name, y = average_user_score, size = average_critic_score), colour = "#8a0303", show.legend = FALSE)+
+        coord_flip()+
+        labs(title = "Resident Evil games showing user and critic scores (size of bubble)",
+             y = "Average user score (out of 10) across platforms",
+             x = "Game")
+    )
+    
+    output$fifa_plot <- renderPlot(
+      fifa_year() %>% 
+      ggplot()+
+        geom_col(aes(y = name, x = sales, fill = platform), position = "dodge")+
+        labs(x = "Sales (millions)",
+             y = "Game Title",
+             title = "Sales of FIFA games released in chosen year by release platfrom")
+    )
 
+    output$fifa_table <- renderTable(
+      fifa_year() %>% 
+      select(-year_of_release)
+    )
+    
+    output$fifa_overall <- renderPlot(
+    CodeClanData::game_sales %>% 
+      filter(str_detect(name, "FIFA")) %>%
+      group_by(year_of_release) %>% 
+      mutate(sales_in_year = sum(sales))%>% 
+      ggplot()+
+      geom_point(aes(x = year_of_release, y = sales_in_year))+
+      geom_smooth(aes(x = year_of_release, y = sales_in_year), method = loess)+
+      labs(title = "Total sales of FIFA games across the years",
+           x = "Year",
+           y = "Total sales of all games released that year (millions)")
+    )
 }
 
 shinyApp(ui, server)
